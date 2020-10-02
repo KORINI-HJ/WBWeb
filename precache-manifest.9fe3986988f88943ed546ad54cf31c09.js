68416c5f12a58513871042c43010ed2af7c7de9c
KORINI-HJ <korini@likelion.org>
2020-10-02 23:07:09 +0900

    Updates

---
+++ precache-manifest.9fe3986988f88943ed546ad54cf31c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fa1c70bccc4de217ae4237e71b2ec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1de541d3d2a92fcdae82",
+    "url": "/static/css/main.fe255856.chunk.css"
+  },
+  {
+    "revision": "faf5d9b942d6dfe81f01",
+    "url": "/static/js/2.f46493fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f46493fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de541d3d2a92fcdae82",
+    "url": "/static/js/main.101f0777.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "017dbdc8267f322ebc1f67ebc3d1283c",
+    "url": "/static/media/unicorn.017dbdc8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/odofortune/precache-manifest.b2ff56c79ff3f84d7b5424906dbc831f.js"
+  "/precache-manifest.9fe3986988f88943ed546ad54cf31c09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/odofortune/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
