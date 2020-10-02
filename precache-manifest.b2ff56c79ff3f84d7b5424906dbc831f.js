9f618d8db6366c7772c76b501185954df21a44fe
KORINI-HJ <korini@likelion.org>
2020-10-02 23:02:57 +0900

    Updates

---
+++ precache-manifest.b2ff56c79ff3f84d7b5424906dbc831f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61fb12044db68accf38b1520a054524",
+    "url": "/odofortune/index.html"
+  },
+  {
+    "revision": "6aaa72251631f5079167",
+    "url": "/odofortune/static/css/main.fe255856.chunk.css"
+  },
+  {
+    "revision": "faf5d9b942d6dfe81f01",
+    "url": "/odofortune/static/js/2.f46493fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/odofortune/static/js/2.f46493fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aaa72251631f5079167",
+    "url": "/odofortune/static/js/main.2104f93d.chunk.js"
+  },
+  {
+    "revision": "aa83e81b4773ee80d6c8",
+    "url": "/odofortune/static/js/runtime-main.a56b799d.js"
+  },
+  {
+    "revision": "017dbdc8267f322ebc1f67ebc3d1283c",
+    "url": "/odofortune/static/media/unicorn.017dbdc8.svg"
+  }
+]);
